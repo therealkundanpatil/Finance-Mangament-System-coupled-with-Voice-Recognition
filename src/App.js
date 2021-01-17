15c15d8c36dad6acb259bf974458fc8de5da55b5
therealkundanpatil <kp28021996@gmail.com>
2021-01-17 16:59:39 -0500

    -Income and Expense achieved using props

--- src/App.js
+++ src/App.js
@@ -1,20 +1,22 @@
 import React from "react";
 import Details from "./components/Details/Details.jsx";
 import { Grid } from "@material-ui/core";
-
+import useStyles from "./styles.js";
 
 function App() {
+
+    const classes = useStyles();
     return (
         <div>
-            <Grid container spacing={0} alignItems="center" justify="center" style={{ height: "100vh" }}>
+            <Grid className={classes.grid} container spacing={0} alignItems="center" justify="center" style={{ height: "100vh" }}>
                 <Grid item xs={12} sm={4}>
-                    <Details/>
+                    <Details title="Income"/>
                 </Grid>
                 <Grid item xs={12} sm={4}>
                     Main
                 </Grid>
                 <Grid item xs={12} sm={4}>
-                    <Details/>
+                    <Details title="Expense"/>
                 </Grid>
             </Grid>
         </div>
--- src/components/Details/styles.js
+++ src/components/Details/styles.js
@@ -6,6 +6,6 @@ export default makeStyles(() => ({
         borderBottom: "10px solid rgba( 0, 255, 0, 0.5 )",
     },
     expense: {
-        borderBottom: "10px solid rgba( 0, 255, 0, 0.5 )",
+        borderBottom: "10px solid rgba( 255, 0, 0, 0.5 )",
     }
 }))
